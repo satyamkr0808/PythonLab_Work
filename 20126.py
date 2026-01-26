44a5b2b705be333ae11458167c639a05a5710b80
Satyam Kumar <satyam.kr151237@gmail.com>
2026-01-26 15:47:20 +0530

    Add files via upload
    Question 1(20/01/2026)

---
+++ 20126.py
@@ -0,0 +1,28 @@
+import pandas as pd
+data=pd.read_csv("medical_records.csv")
+df=pd.DataFrame(data)
+print(df)
+print()
+
+#Checking Null values
+print(df.isnull().sum())
+
+#Fill Null values 
+sugar_median=df['sugar_level'].median()
+df['sugar_level']=df['sugar_level'].fillna(sugar_median)
+print(df)
+print(df.isnull().sum())
+print()
+
+#check outliers
+print(df['bp'].value_counts())
+print()
+
+#Relace high as 140/90
+df['bp'].replace('high','140/90',inplace=True)
+print("\n",df)
+
+print(df['bp'].value_counts())
+
+#Average diagnosis cost
+print(df.groupby('diagnosis')['visit_cost'].mean())
